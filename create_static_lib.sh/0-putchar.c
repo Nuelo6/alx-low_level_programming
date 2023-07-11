490c967c2d8603740e38115ff9119eef134e3f48
Nuelo6 <inwangwa038@stu.ui.edu.ng>
2023-07-11 15:36:20 -0700

    library

--- create_static_lib.sh/0-putchar.c
+++ create_static_lib.sh/0-putchar.c
@@ -1,7 +1,11 @@
+
+
+
 #include <unistd.h>
 
+
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - wirites the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
@@ -11,3 +15,4 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
